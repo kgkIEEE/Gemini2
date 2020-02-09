33c5f0124c351277184166101eec808f82b72dcb
kgkIEEE <kevin.g.kelly@ieee.org>
2020-02-09 07:09:18 -0500

    Added Matlab code to compute satellite position and velocity

--- Matlab/computeSatPosVel.m
+++ Matlab/computeSatPosVel.m
@@ -2,7 +2,7 @@ function [pos, vel] = computeSatPosVel(time, satdata)
     minutesPerDay = 1440.;
     % Compute difference between requested time
     % and TLE epoch
-    deltaTime = convertTleEpochToJdate(satdata.epoch)-time;
+    deltaTime = time - convertTleEpochToJdate(satdata.epoch);
     % SGP4 delta time is in minutes
     % There are 1440 minutes per day
     tsince = deltaTime * minutesPerDay;
